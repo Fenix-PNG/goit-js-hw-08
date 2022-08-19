bba805dffcf4c00e3f03cb0e4de9637b68253af1
Fenix-PNG <67190573+Fenix-PNG@users.noreply.github.com>
2022-08-19 20:07:02 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGallery(array) {
+  return array.reduce(
+    (acc, image) =>
+      acc +
+      `
+          <a class="gallery__item" href="${image.original}">
+          <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>
+          `,
+    ''
+  );
+}
+
+const result = createGallery(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', result);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
